12bb2cd5dad3d8d6e82aa978a7e4bfa7b9de7356
Gabriela <gabriela.priaro@gmail.com>
2024-10-30 16:27:51 +0000

    Implementaçao para mostrar numeros negativos digitados.

--- NumerosNegativosVetores/Program.cs
+++ NumerosNegativosVetores/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace NumerosNegativosVetores
+﻿//obs: utilizando array:
+//Problema "negativos"
+//Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros
+//e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos. 
+
+using System.ComponentModel;
+
+namespace NumerosNegativosVetores
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("NUMEROS NEGATIVOS:");
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
         }
     }
 }
